c2673ba7c83ce1dcc27c6b03f460c00660343a74
GoBrazill <vinicius.daraujos26@gmail.com>
2025-02-25 08:25:30 -0300

    Update QuestãoE_ValorPrestação.java

--- AtividadeFabbri/QuestãoE_ValorPrestação.java
+++ AtividadeFabbri/QuestãoE_ValorPrestação.java
@@ -8,27 +8,26 @@ public class QuestãoE_ValorPrestação {
         Scanner scn = new Scanner (System.in);
 		
         //Variáveis
-		double prestação, valor, juros;
+	double prestação, valor, juros;
+	int mesesSemPagar;
 		
-		int mesesSemPagar;
+	//Entrada
+	System.out.print("Digite o valor da prestação:  ");
+	prestação= scn.nextDouble();
 		
-		//Entrada
-		System.out.print("Digite o valor da prestação:  ");
-		prestação= scn.nextDouble();
+	System.out.print("Digite a taxa de juros:  ");
+	juros = scn.nextDouble();
 		
-		System.out.print("Digite a taxa de juros:  ");
-		juros = scn.nextDouble();
+	System.out.print("Quantos meses ficou sem pagar?  ");
+	mesesSemPagar = scn.nextInt();
 		
-		System.out.print("Quantos meses ficou sem pagar?  ");
-		mesesSemPagar = scn.nextInt();
+	//Cálculo
+	valor = prestação + (prestação * (juros / 100) * mesesSemPagar);
 		
-		//Cálculo
-		valor = prestação + (prestação * (juros / 100) * mesesSemPagar);
+	//Saída
+	System.out.print("O valor a ser pago são " + valor + " reais");
 		
-		//Saída
-		System.out.print("O valor a ser pago são " + valor + " reais");
-		
-		scn.close();
+	scn.close();
 
 	}
 
